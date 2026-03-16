3a644304d523c995f9dd5994dfa86bb7fe72e770
Darshan M R <143525866+7760808482@users.noreply.github.com>
2026-03-16 20:53:56 +0530

    Add files via upload

---
+++ str1.py
@@ -0,0 +1,26 @@
+'''Try the following example in the editor below.
+
+You are given a string S. Your task is to find out if the string contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+Output Format
+
+In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+
+In the second line, print True if S has any alphabetical characters. Otherwise, print False.
+
+In the third line, print True if S has any digits. Otherwise, print False.
+
+In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
+
+In the fifth line, print True if S has any uppercase characters. Otherwise, print False.'''
+def main():
+    s = input()
+
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
+
+if __name__ == '__main__':
+    main()
